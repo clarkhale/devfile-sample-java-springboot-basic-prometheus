a24930828ff33b68849d5d82572f75c26b3b1c19
Clark Hale <chale@redhat.com>
2022-09-27 11:45:08 -0400

    Updated to try to use InetAddress to get hostnames

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.net.InetAddress;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +14,16 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        InetAddress[] peers = InetAddress.getAllByName("java-springboot-headless.jvm-test.svc.cluster.local")
+        StringBuilder s = new StringBuilder("Hello World!\n\nMy peers are:\n")
+
+        for(int i = 0; i < peers.length; i++) {
+            s.append("    " + peers[i].getHostAddress() + "\n")
+        }
+
+        s.append("\n\n");
+
+        return s.toString();
     }
 
     public static void main(String[] args) {
